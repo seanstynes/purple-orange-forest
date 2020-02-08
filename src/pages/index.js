f4765a978439fee83599bfc939f651deeb9b65fe
Sean Stynes <sean@teambrightly.com>
2020-02-08 11:28:31 -0500

    learning

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
 import Layout from '../components/Layout';
 import BlogList from '../components/BlogList';
+import useSiteMetadata from '../static_queries/useSiteMetadata';
 
 export default function IndexPage() {
+	const { title, description } = useSiteMetadata();
+
 	return (
 		<Layout page='home' bgColor='inherit'>
+			{title}
+
 			<section>
 				<BlogList />
 			</section>
