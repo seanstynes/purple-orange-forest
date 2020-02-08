8dbc6d269e1b6393bcd1f7a58fdc4ef74120533f
Sean Stynes <sean@teambrightly.com>
2020-02-08 09:53:44 -0500

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,7 @@ import headerStyles from "../styles/components/header.module.scss"
 
 export default function Header(props) {
   return (
-    <header
-      className={`${headerStyles.header} ${props.page === 'info' &&
-        headerStyles.info_page}`}
-    >
+    <header className={headerStyles.header}>
       <nav
         className={headerStyles.header__nav}
         role="navigation"
@@ -16,22 +13,6 @@ export default function Header(props) {
         <Link to="/">
           <h1>{props.title}</h1>
         </Link>
-        <div>
-          <h1>
-            <Link
-              to={
-                props.page === 'info'
-                  ? "/"
-                  : "/info"
-              }
-              activeClassName={headerStyles.navItemActive}
-            >
-              {props.page === 'info'
-                ? "close"
-                : "info"}
-            </Link>
-          </h1>
-        </div>
       </nav>
     </header>
   )
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,14 +7,7 @@ import layoutStyles from "../styles/components/layout.module.scss"
 export default function Layout(props) {
   const { title, description } = useSiteMetadata()
   return (
-    <section
-      className={`${layoutStyles.layout} ${
-        props.page === "info" && 
-        layoutStyles.info_page}`}
-      style={{
-        backgroundColor: props.bgColor,
-      }}
-    >
+    <section className={layoutStyles.Layout}>
       <Helmet>
         <html lang="en" />
         <title>{title}</title>
